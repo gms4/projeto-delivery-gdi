aec70f9a7a18e642f8376e30208e876bc081aaa2
RenattoPadilha <rspf@cin.ufpe.br>
2021-11-11 16:03:24 -0300

    Separando Criação de Tabelas e Povoamento de pedido_entregue

--- pedido_entregue.sql
+++ pedido_entregue.sql
@@ -11,15 +11,3 @@ CREATE TABLE Pedido_entregue(
     CONSTRAINT pedido_entregue_fk2 FOREIGN KEY (prato_nome, restaurante_cnpj) REFERENCES Prato(nome, cnpj),
     CONSTRAINT pedido_entregue_fk3 FOREIGN KEY (entregador_cpf) REFERENCES Entregador(cpf)
 );
-
-INSERT INTO Pedido_entregue(cliente_cpf, restaurante_cnpj, prato_nome, entregador_cpf, 
-                frete, form_pgmt, data_pedido) VALUES ('1', '11829348569789', 'Azedinho', '6', 4.9, 'crédito', to_date('01/12/2020', 'dd/mm/yy')); 
-
-INSERT INTO Pedido_entregue(cliente_cpf, restaurante_cnpj, prato_nome, entregador_cpf, 
-                frete, form_pgmt, data_pedido) VALUES ('3', '11829348569785', 'Temaki Philadelphia', '7', 5.9, 'débito', to_date('12/10/2020', 'dd/mm/yy'));
-            
-INSERT INTO Pedido_entregue(cliente_cpf, restaurante_cnpj, prato_nome, entregador_cpf, 
-                frete, form_pgmt, data_pedido) VALUES ('4', '11829348569787', 'Casquinha de Siri', '8', 0, 'pix', to_date('12/10/2021', 'dd/mm/yy'));
-
-INSERT INTO Pedido_entregue(cliente_cpf, restaurante_cnpj, prato_nome, entregador_cpf, 
-                frete, form_pgmt, data_pedido) VALUES ('1', '11829348569786', 'Filé a Parmegiana', '6', 4.9, 'dinheiro', to_date('01/12/2021', 'dd/mm/yy')); 
