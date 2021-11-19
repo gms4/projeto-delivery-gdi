2cae915cb60d31c99be04cd5ab8b08b4356bf151
Guilherme Souza <gms4@cin.ufpe.br>
2021-11-19 00:49:55 -0300

    algumas consultas feitas

--- Tabelas e Povoamento (Unificado)/consultas.sql
+++ Tabelas e Povoamento (Unificado)/consultas.sql
@@ -1,18 +1,48 @@
-/* 1. ALTER TABLE */
+/* 1. ALTER TABLE 
+Descrição: Diminuir o tamanho do campo telefone VARCHAR2(255) de 255 para 11, diminuindo assim o uso de memória. */
+ALTER TABLE Telefone_restaurante 
+MODIFY (telefone VARCHAR2(11));
 
-ALTER TABLE Pedido 
-ADD CHECK (horario_fim > horario_inicio); --não existem entregas de um dia pro outro
+/* 2. CREATE INDEX 
+Descrição: Colocar um índice nos atributos cliente e prato do respectivo pedido, para agilizar a busca e facilitar a produção. */
+CREATE INDEX indice_pedido ON Pedido_contem_prato(cliente_cpf, nome_prato);
 
-ALTER TABLE Pedido
-ADD CHECK (entregador_cpf != cliente_cpf);
-/* 2. CREATE INDEX */
-/* 3. INSERT INTO */
-/* 4. UPDATE */
-/* 5. DELETE */
-/* 6 E 7. SELECT FROM WHERE & BETWEEN */
-/* 8. IN */
-/* 9. LIKE */
-/* 10. IS NULL OR IS NOT NULL */
+/* 3. INSERT INTO 
+Descrição: Restaurante adicionou um novo prato ao seu cardápio, vamos colocá-lo na tabela. */
+INSERT INTO Pratos_do_restaurante(nome_prato, cnpj, preco, categoria) VALUES ('Tortinha de Limão Siciliano', '9', 5.00, 'doces');
+
+/* 4. UPDATE 
+Descrição: Inflação chegou, vamos atualizar o valor de um prato no restaurante. */
+UPDATE Pratos_do_restaurante
+SET preco = 80
+WHERE nome_prato = 'Rodízio de Sushi Completo';
+
+/* 5. DELETE 
+Descrição: a parceria entre dois restaurantes acabou, vamos tirá-la da tabela. */
+DELETE FROM Parceria
+WHERE (cnpj_contratante = '5' AND cnpj_contratado = '6');
+
+/* 6 E 7. SELECT FROM WHERE & BETWEEN 
+Descrição: Selecionar o cpf do cliente e a data do pedido de todos os pedidos feitos em 2021. */
+SELECT cliente_cpf, data_pedido FROM Pedido
+WHERE data_pedido
+BETWEEN ('01-JAN-2021 00:00') AND ('31-DEC-2021 23:59');
+
+/* 8. IN 
+Descrição: Mostrar os pratos que foram solicitados pelos restaurantes de cnpj '7' ou '8' e suas respectivas datas. */
+SELECT restaurante_cnpj, nome_prato, data_pedido FROM Pedido_contem_prato
+WHERE restaurante_cnpj IN ('7', '8');
+
+/* 9. LIKE 
+Descrição: Juntar todas as pessoas cadastradas que tem nome começando com a letra J. */
+SELECT nome, cpf FROM Pessoa
+WHERE nome LIKE 'j%';
+
+/* 10. IS NULL OR IS NOT NULL 
+Descrição: Analisar cupons que não estão ligados com nenhum usuário. */
+SELECT descricao, desconto FROM Cupom
+WHERE cliente_cpf IS NULL;
+
 /* 11. INNER JOIN */
 /* 12, 13, 14. MIN, MAX, AVG */
 /* 15. COUNT */
