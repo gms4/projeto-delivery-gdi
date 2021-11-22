47e3211231ea4ea4c893933b0291aa3be6677f31
Guilherme Souza <gms4@cin.ufpe.br>
2021-11-22 15:33:21 -0300

    consultas pl finalizadas

--- Tabelas e Povoamento (Unificado)/consultas_pl.sql
+++ Tabelas e Povoamento (Unificado)/consultas_pl.sql
@@ -39,10 +39,11 @@ BEGIN
         nome_pratos_table(i) := current_row.nome_prato;
         i := i + 1;
     END LOOP;
-    FOR j in 1..i-1 LOOP
+    FOR j IN 1..i-1 LOOP
         DBMS_OUTPUT.PUT_LINE(nome_pratos_table(j));
     END LOOP;
 END nome_pratos_block;
+
 /*3. BLOCO ANÔNIMO
 Descrição: Criando um bloco anônimo com um record de restaurante e printando o nome. */
 DECLARE 
@@ -56,22 +57,106 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(restaurante_01.nome);
 END;
 
-/*4. CREATE PROCEDURE*/
-/*5. CRATE FUNCTION*/
-/*7. %ROWTYPE*/
-/*8. IF ELSIF
-Descrição: Avaliar as notas dos restaurantes, criando uma média
-e deixando uma mensagem para o restaurante de como ele está sendo avaliado no app. */
+/*4 E 7 E 17 E 18. CREATE PROCEDURE, ROWTYPE, CREATE OR REPLACE PACKAGE/BODY. 
+Descrição: Packages com procedures para inserção de elementos do tipo RESTAURANTE E PRATOS na tabela. */
+CREATE OR REPLACE PACKAGE cadastros AS
+PROCEDURE new_rest(aux Restaurante%ROWTYPE);
+PROCEDURE new_prato(
+    p_nome_prato Pratos_do_restaurante.nome_prato%TYPE,
+    p_cnpj Pratos_do_restaurante.cnpj%TYPE,
+    p_preco Pratos_do_restaurante.preco%TYPE,
+    p_categoria Pratos_do_restaurante.categoria%TYPE
+);
+END cadastros;
 
-/*9. CASE WHEN*/
-/*10. LOOP EXIT WHEN*/
-/*11. WHILE LOOP*/
-/*13. SELECT ... INTO*/
-/*14. CURSOR (OPEN, FETCH e CLOSE)*/
-/*15. EXCEPTION WHEN*/
-/*16. USO DE PARÂMETROS (IN, OUT ou IN OUT)*/
-/*17. CREATE OR REPLACE PACKAGE*/
-/*18. CREATE OR REPLACE PACKAGE BODY*/
+CREATE OR REPLACE PACKAGE BODY cadastros AS
+PROCEDURE new_rest(aux Restaurante%ROWTYPE) IS
+BEGIN
+    INSERT INTO Restaurante(cnpj, nome, frete_fixo, cep_endereco, num_endereco)
+            VALUES (aux.cnpj, aux.nome, aux.frete_fixo, aux.cep_endereco, aux.num_endereco);
+END new_rest;
+
+PROCEDURE new_prato(
+    p_nome_prato Pratos_do_restaurante.nome_prato%TYPE,
+    p_cnpj Pratos_do_restaurante.cnpj%TYPE,
+    p_preco Pratos_do_restaurante.preco%TYPE,
+    p_categoria Pratos_do_restaurante.categoria%TYPE
+) IS
+    BEGIN
+        INSERT INTO Pratos_do_restaurante(nome_prato, cnpj, preco, categoria)
+                VALUES (p_nome_prato, p_cnpj, p_preco, p_categoria);
+    END new_prato;
+
+END cadastros;
+
+/*5 E 8 E 13. CREATE FUNCTION, IF/ELSIF E SELECT ... INTO
+Descrição: Função que pega o nome do restaurante e o horário de um pedido feito,
+pega a nota que o cliente atribuiu pra esse pedido e printa mensagens de acordo com a nota. 
+n = 5 -> "Parabéns, mais um pedido muito bem executado!"
+n >= 4 && n < 5 -> "O cliente gostou, mas existe sempre espaço para melhora!" 
+n < 4 -> "Reavalie suas estratégias, a nota não foi boa!" */
+CREATE OR REPLACE FUNCTION avaliacaoPedido (cpf Pedido.cliente_cpf%TYPE, cnpj Pedido.restaurante_cnpj%TYPE, data Pedido.data_pedido%TYPE)
+RETURN VARCHAR2
+IS 
+        nota Pedido.nota_c_r%TYPE;
+        func_out VARCHAR2(255);
+BEGIN 
+        SELECT p.nota_c_r INTO nota
+        FROM Pedido p
+        WHERE p.cliente_cpf = cpf AND p.restaurante_cnpj = cnpj AND p.data_pedido = data;
+
+        IF nota = 5 THEN 
+            func_out := 'Parabéns, mais um pedido muito bem executado!';
+        ELSIF nota >= 4 AND nota < 5 THEN
+            func_out := 'O cliente gostou, mas existe sempre espaço para melhora!';
+        ELSE 
+            func_out := 'Reavalie suas estratégias, a nota não foi boa!';
+        END IF;
+        RETURN func_out;
+END avaliacaoPedido;
+
+/* 9 E 10 E 11 E 14. CASE WHEN, LOOP EXIT WHEN, CURSOR (OPEN, FETCH, CLOSE) E WHILE LOOP
+Descrição: loop que analisa os endereços das pessoas cadastradas, contabilizando de acordo com a cidade.
+Cursor foi usado para pegar a cidade na tabela endereço; */
+DECLARE 
+    count_recife BINARY_INTEGER;
+    count_jaboatao BINARY_INTEGER;
+    count_cabo BINARY_INTEGER;
+    count_outros BINARY_INTEGER;
+    i BINARY_INTEGER;
+    q BINARY_INTEGER;
+    aux_cidade Endereco.cidade%TYPE;
+
+    CURSOR c_endereco IS
+        SELECT E.cidade
+        FROM Endereco E;
+BEGIN
+    OPEN c_endereco;
+    count_recife := 0;
+    count_jaboatao := 0;
+    count_cabo := 0;
+    count_outros := 0;
+    i := 1;
+    SELECT COUNT(*) INTO q FROM Endereco;
+    WHILE i < q LOOP
+        FETCH c_endereco INTO aux_cidade;
+        EXIT WHEN c_endereco%NOTFOUND;
+        CASE aux_cidade
+            WHEN 'recife' THEN
+                count_recife := count_recife + 1;
+            WHEN 'jaboatão dos guararapes' THEN
+                count_jaboatao := count_jaboatao + 1;
+            WHEN 'cabo de santo agostinho' THEN
+                count_cabo := count_cabo + 1;
+            ELSE
+                count_outros := count_outros + 1;
+        END CASE;
+        i := i + 1;
+    END LOOP;
+    CLOSE c_endereco;
+    DBMS_OUTPUT.PUT_LINE('Recife: ' || count_recife || ', Jaboatão: ' || count_jaboatao || ', Cabo: ' || count_cabo || ', Outros: ' || count_outros);
+END;
+
 /*19. CREATE OR REPLACE TRIGGER (COMANDO​)
 Descrição: Trigger ativado quando existe a tentativa de se fazer um pedido fora do horário definido pelo aplicativo. */
 CREATE OR REPLACE TRIGGER fora_do_horario
@@ -79,14 +164,15 @@ BEFORE INSERT ON Pedido
 DECLARE
     pedido_fora_do_horario EXCEPTION;
 BEGIN 
-    IF TO_(SYSDATE, 'HH24') > 23 OR TO_NUMBER(SYSDATE, 'HH24') < 8
+    IF TO_NUMBER(SYSDATE, 'HH24') > 23 OR TO_NUMBER(SYSDATE, 'HH24') < 8
     THEN 
         RAISE pedido_fora_do_horario;
     END IF;
 EXCEPTION 
 WHEN pedido_fora_do_horario THEN
-    Raise_application_error(-20202, 'FORA DO HORARIO-' || 'Restaurantes operam entre 8 e 23h.');
+    Raise_application_error(-20202, 'FORA DO HORARIO-' || 'Restaurantes operam entre 8 e 23h. Tente novamente em outro horário.');
 END;
+
 /*20. CREATE OR REPLACE TRIGGER (LINHA​)
 Descrição: Trigger ativado quando se tenta inserir um cupom de valor de desconto negativo. */
 CREATE OR REPLACE TRIGGER desconto_negativo
